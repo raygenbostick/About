e758f9cf9a3baa91be0f34ed48dd527e91b77c69
Raygen Bostick <raygenbostick@Raygens-MacBook-Pro.local>
2022-10-04 14:36:25 -0400

    adding final project to github

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+/*
+  Student Name: Raygen Bostick
+  File Name:contact.html
+  Date:02/17/2022
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var answer = document.getElementByID("nav-links");
+    var heading = document.getElementByID("ffc-logo");
+        if (menu.style.display === "block" && logo.style.display === "none" {
+            menu.style.display = "none";
+            logo.style.display = "block";
+        }else {
+            menu.style.display = "block";
+            logo.style.display = "none";
+        }
+}
+
+function content() {
+    var text = document.getElementById("new");
+    text.textContent =
+    text.style.color =
+    text.style.fontSize =
+   
+}
+
